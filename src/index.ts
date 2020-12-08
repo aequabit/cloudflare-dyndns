bab54924300e296532429e94e3323b5f363ca992
aequabit <contact@aequabit.de>
2020-12-08 14:56:52 +0100

    Only update DNS entry on start and when IP changed

--- src/index.ts
+++ src/index.ts
@@ -8,35 +8,48 @@ import Logger from './lib/Logger';
 // CloudFlare has a limit of 1200 calls every five minutes
 const UPDATE_INTERVAL = 10 * 1000;
 
+const cf = new CloudFlare(config);
+let lastIp = null;
+
 const getIp = async (): Promise<string> => {
-    const response = await request({ uri: 'https://wtfismyip.com/json' });
+    const response = await request({ uri: 'https://api.myip.com' });
 
     try {
-        return JSON.parse(response).YourFuckingIPAddress;
+        return JSON.parse(response).ip;
     } catch (err) {
         return null;
     }
 }
 
+const updateDnsEntry = async () => {
+    let ip = await getIp();
+
+    // Failed to get IP
+    if (ip === null) {
+        Logger.error('app', 'Failed to get IP address');
+        return;
+    }
+
+    // IP didn't change (will run once because lastIp isn't set initially)
+    if (ip === lastIp)
+        return;
+
+    if (lastIp !== null)
+        Logger.info('app', 'IP change from %s to %s', lastIp, ip);
+
+    lastIp = ip;
+
+    try {
+        await cf.updateRecord(ip);
+
+        Logger.info('cloudflare', 'Updated IP for %s to %s', cf.getConfig().domain, ip);
+    } catch (err) {
+        Logger.error('cloudflare', 'Error while updating DNS record IP: %s', err.message);
+    }
+};
+
 (async (): Promise<void> => {
-    Logger.info('cloudflare', 'Started DNS updater');
+    Logger.info('app', 'Started');
 
-    const cf = new CloudFlare(config);
-
-    setInterval(async () => {
-        const ip = await getIp();
-
-        if (ip === null) {
-            Logger.error('ip', 'Failed to get IP address');
-            return;
-        }
-
-        try {
-            await cf.updateRecord(ip);
-            Logger.info('cloudflare', 'Updated IP for %s to %s', cf.getConfig().domain, ip);
-        } catch (err) {
-            Logger.error('cloudflare', 'Error while getting external IP: %s', err.message);
-        }
-
-    }, UPDATE_INTERVAL);
+    setInterval(async () => await updateDnsEntry(), UPDATE_INTERVAL);
 })();
